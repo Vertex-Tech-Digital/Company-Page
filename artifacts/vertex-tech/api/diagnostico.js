ccf079a24ebab745c13c37a87d8d12fbc21ce903
Carlos Arias <carlosariasmorales36@gmail.com>
2026-07-14 16:03:20 -0400

    fix: solucionar dependencias e importaciones locales para despliegue en Vercel

--- artifacts/vertex-tech/api/diagnostico.js
+++
@@ -1,5 +0,0 @@
-require("tsx/cjs");
-const path = require("path");
-const tsFile = path.join(__dirname, "diagnostico.ts");
-delete require.cache[require.resolve(tsFile)];
-module.exports = require(tsFile).default;
